ca097ce7d79f7c160dcce427ac75ec959daf83d7
Irina <sepetovairina@gmail.com>
2022-08-17 10:56:19 +0300

    HW3

---
+++ src/main/java/org/example/lesson4/NumberIsNegative.java
@@ -0,0 +1,12 @@
+package org.example.lesson4;
+
+public class NumberIsNegative extends Exception{
+    String s;
+    public NumberIsNegative(String s) {
+        this.s = s;
+    }
+
+    public static String main(String s) {
+        return s;
+    }
+}
